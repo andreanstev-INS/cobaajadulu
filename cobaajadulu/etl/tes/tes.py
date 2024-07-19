5c485ea6d315ee40e0529e1fde851841a6f5abd8
andreanstev2023 <andrean.stevanus@insignia.co.id>
2024-07-19 11:00:31 +0700

    .

--- cobaajadulu/etl/tes/tes.py
+++ cobaajadulu/etl/tes/tes.py
@@ -1,2 +1,14 @@
-def cobaprint(text):
-    print(text)
+class Printer:
+    """A simple class to print text."""
+
+    def __init__(self):
+        """Initializes the Printer class."""
+        pass
+
+    def cobaprint(self, text):
+        """Prints the given text.
+
+        Args:
+            text (str): The text to print.
+        """
+        print(text)
